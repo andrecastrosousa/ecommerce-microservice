35b929a59ab9e0124204f430fab727f141c93265
André Sousa <andre.castro.sousa@gmail.com>
2023-09-11 16:25:12 +0000

    add: get and list orders in order service unit test

--- order-service/src/test/java/academy/mindswap/orderservice/service/OrderServiceUnitTest.java
+++ order-service/src/test/java/academy/mindswap/orderservice/service/OrderServiceUnitTest.java
@@ -3,14 +3,14 @@ package academy.mindswap.orderservice.service;
 
 import academy.mindswap.orderservice.model.Order;
 import academy.mindswap.orderservice.repository.OrderRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -22,41 +22,156 @@ import static org.mockito.Mockito.*;
 class OrderServiceUnitTest {
 
     @InjectMocks
-    OrderServiceImpl orderService;
+    private OrderServiceImpl orderService;
 
     @Mock
-    OrderRepository orderRepository;
+    private OrderRepository orderRepository;
 
-    @Test
-    void shouldGetOrder() {
-        Order order = Order.builder()
-                .id(1L)
-                .total(0.0)
-                .orderItemList(new ArrayList<>())
-                .build();
+    @Nested
+    @Tag("listOrders")
+    @DisplayName("List orders")
+    class ListOrdersValidations {
+        @Test
+        @DisplayName("List orders successfully")
+        void shouldListOrders() {
+            Order order1 = Order.builder()
+                    .id(1L)
+                    .total(0.0)
+                    .orderItemList(new ArrayList<>())
+                    .build();
 
-        when(orderRepository.findById(1L)).thenReturn(Mono.just(order));
-        Mono<Order> userMono = orderService.get(1L);
+            Order order2 = Order.builder()
+                    .id(1L)
+                    .total(0.0)
+                    .orderItemList(new ArrayList<>())
+                    .build();
 
-        StepVerifier
-                .create(userMono)
-                .consumeNextWith(newOrder -> {
-                    Assertions.assertEquals(newOrder, order);
-                })
-                .verifyComplete();
+            when(orderRepository.findAll()).thenReturn(Flux.just(order1, order2));
+            Flux<Order> orderFlux = orderService.listAll();
 
-        verify(orderRepository, times(1)).findById(1L);
+            StepVerifier
+                    .create(orderFlux)
+                    .expectNextCount(2L)
+                    .expectComplete()
+                    .verify();
+
+            verify(orderRepository, times(1)).findAll();
+        }
     }
 
-    @Test
-    void shouldGetOrderNotFound() {
-        when(orderRepository.findById(1L)).thenReturn(Mono.empty());
-        Mono<Order> userMono = orderService.get(1L);
-        StepVerifier
-                .create(userMono)
-                .expectErrorMatches(throwable -> throwable instanceof ResponseStatusException)
-                .verify();
+    @Nested
+    @Tag("getOrder")
+    @DisplayName("Get order")
+    class GetOrderValidations {
+        @Test
+        @DisplayName("Get order successfully")
+        void shouldGetOrder() {
+            Order order = Order.builder()
+                    .id(1L)
+                    .total(0.0)
+                    .orderItemList(new ArrayList<>())
+                    .build();
 
-        verify(orderRepository, times(1)).findById(1L);
+            when(orderRepository.findById(1L)).thenReturn(Mono.just(order));
+            Mono<Order> orderMono = orderService.get(1L);
+
+            StepVerifier
+                    .create(orderMono)
+                    .consumeNextWith(newOrder -> {
+                        Assertions.assertEquals(newOrder, order);
+                    })
+                    .verifyComplete();
+
+            verify(orderRepository, times(1)).findById(1L);
+        }
+
+        @Test
+        @DisplayName("Get a not found order")
+        void shouldGetOrderNotFound() {
+            when(orderRepository.findById(1L)).thenReturn(Mono.empty());
+            Mono<Order> userMono = orderService.get(1L);
+            StepVerifier
+                    .create(userMono)
+                    .expectErrorMatches(throwable -> throwable instanceof ResponseStatusException)
+                    .verify();
+
+            verify(orderRepository, times(1)).findById(1L);
+        }
+    }
+
+    @Nested
+    @Tag("createOrder")
+    @DisplayName("Create order")
+    class CreateOrderValidations {
+        @Test
+        @DisplayName("Create order successfully")
+        void shouldCreateOrder() {
+            Order order = Order.builder()
+                    .id(1L)
+                    .total(0.0)
+                    .orderItemList(new ArrayList<>())
+                    .build();
+
+            when(orderRepository.save(order)).thenReturn(Mono.just(order));
+            Mono<Order> orderMono = orderService.create(order);
+
+            StepVerifier
+                    .create(orderMono)
+                    .consumeNextWith(newOrder ->
+                            Assertions.assertEquals(newOrder, order))
+                    .verifyComplete();
+
+            verify(orderRepository, times(1)).save(order);
+        }
+    }
+
+    @Nested
+    @Tag("updateOrder")
+    @DisplayName("Update order")
+    class UpdateOrderValidations {
+        @Test
+        @DisplayName("Update order successfully")
+        void shouldUpdateOrder() {
+            Order order = Order.builder()
+                    .id(1L)
+                    .total(0.0)
+                    .orderItemList(new ArrayList<>())
+                    .build();
+
+            when(orderRepository.findById(order.getId())).thenReturn(Mono.just(order));
+            when(orderRepository.save(order)).thenReturn(Mono.just(order));
+            Mono<Order> orderMono = orderService.update(order.getId(), order);
+
+            StepVerifier
+                    .create(orderMono)
+                    .consumeNextWith(newOrder ->
+                            Assertions.assertEquals(newOrder, order))
+                    .verifyComplete();
+
+            verify(orderRepository, times(1)).findById(order.getId());
+            verify(orderRepository, times(1)).save(order);
+        }
+
+        @Test
+        @DisplayName("Update a not found order")
+        void shouldUpdateOrderNotFound() {
+            Order order = Order.builder()
+                    .id(1L)
+                    .total(0.0)
+                    .orderItemList(new ArrayList<>())
+                    .build();
+
+            when(orderRepository.findById(order.getId())).thenReturn(Mono.empty());
+
+            Mono<Order> orderMono = orderService.update(order.getId(), order);
+
+            StepVerifier
+                    .create(orderMono)
+                    .expectErrorMatches(throwable -> throwable instanceof ResponseStatusException)
+                    .verify();
+
+            verify(orderRepository, times(1)).findById(order.getId());
+            verify(orderRepository, times(0)).save(order);
+        }
     }
 }
