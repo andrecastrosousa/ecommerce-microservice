aac449bb587fa247b586531f2f278a09f720cee4
André Sousa <andre.castro.sousa@gmail.com>
2023-09-17 12:08:02 +0000

    add: auth service for jwt with redis

--- order-service/src/main/java/academy/mindswap/orderservice/config/SecurityConfiguration.java
+++ order-service/src/main/java/academy/mindswap/orderservice/config/SecurityConfiguration.java
@@ -2,10 +2,8 @@ package academy.mindswap.orderservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -15,20 +13,6 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http
-                .oauth2Client(Customizer.withDefaults())
-                .oauth2Login(Customizer.withDefaults())
-                .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer
-                        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
-                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
-                        authorizationManagerRequestMatcherRegistry
-                                .requestMatchers("/unauthorized", "/oauth2/**", "/login/**")
-                                .permitAll()
-                                .anyRequest()
-                                .fullyAuthenticated()
-                ).logout(logout -> logout
-                        .logoutSuccessUrl("http://localhost:8091/realms/springEcommerce/protocol/openid-connect/logout?redirect_uri=http://localhost:8091/"));
 
         return http.build();
     }
