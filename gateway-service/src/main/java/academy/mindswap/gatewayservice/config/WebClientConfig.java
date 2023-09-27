da4beef1d527890aa014ae4772279f5cdd3def11
André Sousa <andre.castro.sousa@gmail.com>
2023-09-27 17:05:02 +0000

    add: auth service base url to gateway service

--- gateway-service/src/main/java/academy/mindswap/gatewayservice/config/WebClientConfig.java
+++ gateway-service/src/main/java/academy/mindswap/gatewayservice/config/WebClientConfig.java
@@ -1,14 +1,17 @@
 package academy.mindswap.gatewayservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
+    @Value("${auth-service.base.url")
+    private String authServiceBaseUrl;
 
     @Bean
     public WebClient webClient() {
-        return WebClient.create("http://localhost:8082");
+        return WebClient.create(authServiceBaseUrl);
     }
 }
